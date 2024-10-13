a26e412641c156640e78895b00cd155e1fc5ac17
Puginator5000 <emmaminime3662@gmail.com>
2024-10-13 14:38:08 -0700

    Done

--- Problem1.java
+++ Problem1.java
@@ -1,14 +1,13 @@
-import java.util.Scanner;
 
 public class Problem1
 {
   public static void main(String[] args)
   {
-    int a = Integer.MAX_VALUE();
-    a + 1;
+    Integer a = Integer.MAX_VALUE;
+    a = a + 1;
     System.out.println(a);
-    int b = Integer.MIN;
-    b - 1;
-    System.out.println('b');
+    Integer b = Integer.MIN_VALUE;
+    b = b - 1;
+    System.out.println(b);
   }
 }
